693dc68820a71f70992310fc7b515c0de4289167
ted <linyc1983@gmail.com>
2015-05-08 17:16:50 +0800

    add event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507103023) do
+ActiveRecord::Schema.define(version: 20150508063843) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.boolean  "is_public",   limit: 1
+    t.integer  "capacity",    limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name",       limit: 255
